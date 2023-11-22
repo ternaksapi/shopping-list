cde582aba29af1f7a46b9e70bad94a7bbfa823d8
ternaksapi <yusufhaikaln7@gmail.com>
2023-11-22 15:39:31 +0700

    minor-changes

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('auth/', include('authentication.urls'))
 ]
